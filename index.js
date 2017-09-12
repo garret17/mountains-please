9d80fc6ad603ecf18ccce85924bd51387450afd9
Matt Price <matt.price@utoronto.ca>
2017-09-12 07:09:24 -0400

    further js imprivements
    better menubar options.  docstrings.

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const htmlToText = require('html-to-text');
 const menubar = require("menubar");
 const {ipcMain} = require("electron");
 const getRandomRecipe = require("./lib/get-recipe");
-const mb = menubar({showDockIcon: true, width: 800, height: 800});
-const BrowserWindow = ipcMain.BrowserWindow;
+const mb = menubar({showDockIcon: true, width: 800, height: 800, tooltip: "Mountains, Please!", windowPosition: "center"});
 
 
 
 mb.on("ready", () => {
   console.log("ready!");
-});
+  // not clear how to open on application start
+  //mb.createWindow();
+  });
 
+// for debugging later
 // mb.on("after-create-window", () => {
 //   mb.window.openDevTools();
 // });
